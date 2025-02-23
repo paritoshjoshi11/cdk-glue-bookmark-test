2ff3e25b2302c66ebfd8194010757bc761fc437a
paritosh <paritoshjoshi11@gmail.com>
2025-02-23 15:21:01 +0530

    Initial commit

---
+++ test/cdk-glue-bookmark-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGlueBookmarkTest from '../lib/cdk-glue-bookmark-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-glue-bookmark-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGlueBookmarkTest.CdkGlueBookmarkTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
